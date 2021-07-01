3904a980e13fa43a2f0b153f5c740ee1616e355b
kcdoggo <cdoggokk@gmail.com>
2021-07-01 09:18:46 +0300

    nodeNreact

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://cdoggo:tnwlssl6@boilerplate.no86a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: false }). then(()=> console.log('MongoDB Connected'))
+    .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('so annoying')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
